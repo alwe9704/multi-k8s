3b19abb2e2e5a62d4dd13b373bca18af5d6c9946
Alexander Westman <alex@pond.se>
2021-03-21 20:20:18 +0100

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
-docker build -t alwe9704/multi-client -f ./client/Dockerfile ./client
-docker build -t alwe9704/multi-server -f ./server/Dockerfile ./server
-docker build -t alwe9704/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t alwe9704/multi-client:latest -t alwe9704/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t alwe9704/multi-server:latest -t alwe9704/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t alwe9704/multi-worker:latest -t alwe9704/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push alwe9704/multi-client
-docker push alwe9704/multi-server
-docker push alwe9704/multi-worker
+docker push alwe9704/multi-client:$SHA
+docker push alwe9704/multi-worker:$SHA
+docker push alwe9704/multi-server:$SHA
+
+docker push alwe9704/multi-client:latest
+docker push alwe9704/multi-server:latest
+docker push alwe9704/multi-worker:latest
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=alwe9704/multi-server
+kubectl set image deployments/server-deployment server=alwe9704/multi-server:$SHA
+kubectl set image deployments/client-deployment client=alwe9704/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=alwe9704/multi-worker:$SHA
